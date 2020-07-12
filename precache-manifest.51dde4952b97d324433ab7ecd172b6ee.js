4b64facaf97c55bca7131092f2aa900a4cb3caf0
Shashwat pal <shashwatpal.che18@itbhu.ac.in>
2020-07-12 15:53:24 +0530

    Updates

---
+++ precache-manifest.51dde4952b97d324433ab7ecd172b6ee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f2a887f96f2bd4e6d21af3dbe012338",
+    "url": "/her-gallery/index.html"
+  },
+  {
+    "revision": "8945f818f4808d7331ac",
+    "url": "/her-gallery/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "29056b6c2f95e1053d52",
+    "url": "/her-gallery/static/css/main.1bb7ada6.chunk.css"
+  },
+  {
+    "revision": "8945f818f4808d7331ac",
+    "url": "/her-gallery/static/js/2.964bd2f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/her-gallery/static/js/2.964bd2f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29056b6c2f95e1053d52",
+    "url": "/her-gallery/static/js/main.b90a09a1.chunk.js"
+  },
+  {
+    "revision": "ac21e1f6b53a1b440d7d",
+    "url": "/her-gallery/static/js/runtime-main.76c526f1.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/her-gallery/static/media/SEGA.12a3db8b.woff"
+  }
+]);
